0b820b3d9c0c72b4d777fcdfe5cd1690ccca72a8
Konstantin-AKV <abdulaev-kostya@mail.ru>
2022-07-27 01:36:51 +0300

    Создаем два массива

--- Решение задачи (C#)/Program.cs
+++ Решение задачи (C#)/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] {"321", "62", "hello", "world", "FOR"};
+string[] array2 = new string[array1.Length];
